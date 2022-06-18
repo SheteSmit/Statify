4777c263086f427886970a7145609436fde07040
SheteSmit <shetesmit@gmail.com>
2022-06-17 20:35:25 -0400

    Firebase

--- frontend/src/pages/Start.js
+++ frontend/src/pages/Start.js
@@ -5,13 +5,13 @@ import classes from './Start.module.css'
 export default function Start() {
     
     const buttonHandler = () => {
-        window.location.replace("https://accounts.spotify.com/authorize?client_id=8d8af266e27f4ca4b997bf3b1d9def67&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Fscores&scope=user-library-read")
+        window.location.replace("https://accounts.spotify.com/authorize?client_id=8d8af266e27f4ca4b997bf3b1d9def67&response_type=code&redirect_uri=https%3A%2F%2Fstatify-447ae.web.app%2Fscores&scope=user-library-read")
     }
 
     return (
     <>
         
-        <a href='/temp'>CLICK</a>
+        <a href='/temp'>YOYOYOYOYO</a>
         <body className={classes.body}>
             <div className={classes.content} title="Statify">
             Statify
