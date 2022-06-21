038a9db05d8c34d5ad7330bf4bb789de7e06758d
SheteSmit <shetesmit@gmail.com>
2022-06-20 22:12:44 -0400

    Grid Layout

--- frontend/src/components/Playlist.js
+++ frontend/src/components/Playlist.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from "./Playlist.module.css"
 import Card from '../ui/Card'
+import grids from '../ui/Grid.module.css'
 
 export default function Playlist(props) {
 
@@ -9,70 +10,79 @@ export default function Playlist(props) {
     
     <li className={classes.item}>
       <Card>
-        <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
-        </div>
-        <div className={classes.content}>
-            <h2>{props.title}</h2>
-        </div>
-        {num.style.setProperty('--danceBox', props.dance)}
-        <div className={classes.box_dance}>
-          <div className={classes.percent}>
-            <svg>
-              <circle cx="70" cy="70" r="70"></circle>
-              <circle cx="70" cy="70" r="70"></circle>
-              
-            </svg>
-            <div className={classes.num}>
-              <h2>{props.dance}<span>%</span></h2>
+        <div className={grids.grid}>
+          <div className={grids.square.fullImg}>
+            <div className={classes.image}>
+              <img src={props.image} alt={props.title} />
+            </div>
+            <div>
+                <h2 className={classes.content}>{props.title}</h2>
             </div>
           </div>
-          <h2 className={classes.text}>Danceability</h2>
-        </div>
+            <div className={grids.square}>
+              {num.style.setProperty('--danceBox', props.dance)}
+              <div className={grids.square.items}>
+                <div className={classes.box_dance}>
+                  <div className={classes.percent}>
+                    <svg>
+                      <circle cx="70" cy="70" r="70"></circle>
+                      <circle cx="70" cy="70" r="70"></circle>
+                      
+                    </svg>
+                    <div className={classes.num}>
+                      <h2>{props.dance}<span>%</span></h2>
+                    </div>
+                  </div>
+                  <h2 className={classes.text}>Danceability</h2>
+                </div>
 
-        {num.style.setProperty('--energyBox', props.energy)}
-        <div className={classes.box_energy}>
-          <div className={classes.percent}>
-            <svg>
-              <circle cx="70" cy="70" r="70"></circle>
-              <circle cx="70" cy="70" r="70"></circle>
-              
-            </svg>
-            <div className={classes.num}>
-              <h2>{props.energy}<span>%</span></h2>
-            </div>
-          </div>
-          <h2 className={classes.text}>Energy</h2>
-        </div>
+                {num.style.setProperty('--energyBox', props.energy)}
+                <div className={classes.box_energy}>
+                  <div className={classes.percent}>
+                    <svg>
+                      <circle cx="70" cy="70" r="70"></circle>
+                      <circle cx="70" cy="70" r="70"></circle>
+                      
+                    </svg>
+                    <div className={classes.num}>
+                      <h2>{props.energy}<span>%</span></h2>
+                    </div>
+                  </div>
+                  <h2 className={classes.text}>Energy</h2>
+                </div>
+              </div>
+              {num.style.setProperty('--acousticBox', props.acoustic)}
+              <div className={grids.square.items}>
+                <div className={classes.box_acoustic}>
+                  <div className={classes.percent}>
+                    <svg>
+                      <circle cx="70" cy="70" r="70"></circle>
+                      <circle cx="70" cy="70" r="70"></circle>
+                      
+                    </svg>
+                    <div className={classes.num}>
+                      <h2>{props.acoustic}<span>%</span></h2>
+                    </div>
+                  </div>
+                  <h2 className={classes.text}>Acousticness</h2>
+                </div>
 
-        {num.style.setProperty('--acousticBox', props.acoustic)}
-        <div className={classes.box_acoustic}>
-          <div className={classes.percent}>
-            <svg>
-              <circle cx="70" cy="70" r="70"></circle>
-              <circle cx="70" cy="70" r="70"></circle>
-              
-            </svg>
-            <div className={classes.num}>
-              <h2>{props.acoustic}<span>%</span></h2>
+                {num.style.setProperty('--valenceBox', props.valence)}
+                <div className={classes.box_valence}>
+                  <div className={classes.percent}>
+                    <svg>
+                      <circle cx="70" cy="70" r="70"></circle>
+                      <circle cx="70" cy="70" r="70"></circle>
+                      
+                    </svg>
+                    <div className={classes.num}>
+                      <h2>{props.valence}<span>%</span></h2>
+                    </div>
+                  </div>
+                  <h2 className={classes.text}>Valence</h2>
+              </div>
             </div>
           </div>
-          <h2 className={classes.text}>Acousticness</h2>
-        </div>
-
-        {num.style.setProperty('--valenceBox', props.valence)}
-        <div className={classes.box_valence}>
-          <div className={classes.percent}>
-            <svg>
-              <circle cx="70" cy="70" r="70"></circle>
-              <circle cx="70" cy="70" r="70"></circle>
-              
-            </svg>
-            <div className={classes.num}>
-              <h2>{props.valence}<span>%</span></h2>
-            </div>
-          </div>
-          <h2 className={classes.text}>Valence</h2>
         </div>
       </Card>
     </li>
